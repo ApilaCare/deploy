ca40e7c0b408cbd530c9aa0cfd0cece2df12dd1d
nenad <nesa993@gmail.com>
2016-05-18 22:17:47 +0200

    added te api and deploy frontend and did a basic setup'

---
+++ app_api/apila.js
@@ -0,0 +1,59 @@
+require('dotenv').load();
+var debug = require('debug')('Express4');
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var cors = require('cors');
+
+require('./models/db');
+require('./config/passport');
+
+// (commented out because routes in the server are not used)
+// var routes = require('./app_server/routes/index');
+var routesApi = require('./routes/index');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+
+
+
+//so we can call our api from another server
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With' );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/api', routesApi);
+app.use('/files', express.static(__dirname + 'upload_storage'));
+
+app.set('port', process.env.PORT || 3300);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+module.exports = app;
