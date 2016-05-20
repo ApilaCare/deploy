041eda5148a8cb3bfe22d01d300fe53fff865eb0
nenad <nesa993@gmail.com>
2016-05-20 18:23:27 +0200

    changed env var name

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = "mongodb://localhost/loc8r";
 if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGODB_URI;
 }
 
 console.log(dbURI);
