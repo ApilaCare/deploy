f9cac59cbdec2b2aa637fce7c06fd6da78ee8517
nenad <nesa993@gmail.com>
2016-05-20 17:53:44 +0200

    error handling and swtiching routing to angular

--- apila.js
+++ apila.js
@@ -26,32 +26,14 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 
 
-
-//so we can call our api from another server
-// Add headers
-// app.use(function (req, res, next) {
-//
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With' );
-//
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//     // Pass to next layer of middleware
-//     next();
-// });
-
 app.use('/api', routesApi);
 app.use('/files', express.static(__dirname + 'upload_storage'));
 
 app.use(express.static(path.join(__dirname, 'app_client')));
+
+app.get('*', function(req, res) {
+  res.sendfile((path.join(__dirname, 'app_client/index.html')));
+});
 
 app.set('port', process.env.PORT || 3300);
 
