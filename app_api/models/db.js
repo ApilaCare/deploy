2d4284d75bb48e2b399813e8e51f4e59673f67ab
nenad <nesa993@gmail.com>
2016-05-20 18:18:22 +0200

    debugg info

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -5,7 +5,11 @@ if (process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGOLAB_URI;
 }
 
+console.log(dbURI);
+
 mongoose.connect(dbURI);
+
+console.log(mongoose);
 
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
