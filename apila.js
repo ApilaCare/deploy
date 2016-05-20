fb2e299d061bdebcbb7d8e67a0fcf87f0d198967
nenad <nesa993@gmail.com>
2016-05-20 18:01:34 +0200

    without node.js error handling

--- apila.js
+++ apila.js
@@ -41,50 +41,5 @@ var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 
-/* !!!! ERROR HANDLERS !!!! */
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-// Catch unauthorised errors
-app.use(function(err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401);
-        res.json({
-            "message": err.name + ": " + err.message
-        });
-    }
-
-    res.json({
-        "message": err.name + ": " + err.message
-    });
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 module.exports = app;
