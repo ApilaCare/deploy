55bb3112456de2661a7eb559ece4f92e0109792d
KyleRiggen <kyle.riggenbach@gmail.com>
2016-06-17 14:28:35 -0600

    trying to fix connection to database

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
 
-var dbURI = "mongodb://localhost/loc8r"
+var dbURI = process.env.MONGODB_URI;
 
 if (process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGODB_URI;
