6c58c33c8210e5994228b5b650282af6e06df84f
KyleRiggen <kyle.riggenbach@gmail.com>
2016-06-23 08:04:57 -0600

    k nevermind didnt work

--- apila.js
+++ apila.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
+/*
 if(process.env.NODE_ENV === 'production') {
   app.get('*', function(req, res, next) {
     if(req.headers['x-forwarded-proto'] != 'https') {
@@ -34,6 +35,7 @@ if(process.env.NODE_ENV === 'production') {
       }
   });
 }
+*/
 
 app.use('/api', routesApi);
 app.use('/files', express.static(__dirname + 'upload_storage'));
