9f38e151af32ce816a7e0c7480c2293c17f4d12b
nenad <nesa993@gmail.com>
2016-07-07 19:45:51 +0200

    syntax error fix

--- app_api/services/email.js
+++ app_api/services/email.js
@@ -1,25 +1,15 @@
 (function() {
   'use strict';
 
-
   
-
-
-  var mailOptions = {
-    from: '',
-    to: '',
-    subject: '',
-    text: '',
-    html: ''
-};
 
   module.exports.sendMail = function(from, to, subject, text, callback) {
 
-  
+   
   }
 
   module.exports.sendForgotPassword = function(from, to, token, host, callback) {
-    
+
   }
 
 })();
