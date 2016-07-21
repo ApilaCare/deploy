2ae1db65a30b379d8e5794648012ea766ef0fc1c
KyleRiggen <kyle.riggenbach@gmail.com>
2016-07-21 11:27:28 -0600

    remove test file

--- app_api/models/calendar.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var calendarSchema = new mongoose.Schema({
-    appointments: [String], // _id of appointments
-    residentCarePlanReview: [String], // _id of residents
-    issueDueDate: [String], // _id of issues
-});
-
-mongoose.model('Calendar', calendarSchema);
