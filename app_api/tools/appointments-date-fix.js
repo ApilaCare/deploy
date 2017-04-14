3fd1efbe5e167e54e1482a5bf5a2c3957ad787fe
KyleRiggen <kyle.riggenbach@gmail.com>
2017-04-14 10:38:19 -0600

    new stuff

---
+++ app_api/tools/appointments-date-fix.js
@@ -0,0 +1,28 @@
+require('../models/db');
+
+const mongoose = require('mongoose');
+const Appoint = mongoose.model('Appointment');
+
+const moment = require('moment');
+
+const asyncLib = require('async');
+
+(() => {
+  Appoint.find({}).exec(function(err, appointments) {
+    if(!err) {
+
+      console.log("started updating appointments");
+
+      asyncLib.each(appointments, function(appointment) {
+        appointment.currMonth = moment(appointment.appointmentDate).format("YYYY M");
+
+       appointment.save();
+      });
+
+      console.log("appointments updated");
+
+    } else {
+      console.log("Error while finding appointments");
+    }
+  });
+})();
